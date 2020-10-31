0fe9cd92aa8d6b7447223725c9a68c27216c1b8b
Undefinaids <nicolas.lemaire@epitech.eu>
2020-10-31 15:02:05 +0100

    Implement the info action

--- app/actions/info.py
+++ app/actions/info.py
@@ -8,6 +8,11 @@ class Info(Action):
         super().__init__('info')
 
     def execute(self, hpk_api, args):
-        print("Executing {} action".format(self.label))
-        print("TODO")
+        me = hpk_api.me_service.me()
+        attributes = me['data']['attributes']
+        print("Email: {}".format(attributes['email']))
+        print("Prénom: {}".format(attributes['firstName']))
+        print("Nom: {}".format(attributes['lastName']))
+        print("Adresse email validée: {}".format(
+              ("oui" if attributes['emailAddressValidated'] else "non")))
         return
