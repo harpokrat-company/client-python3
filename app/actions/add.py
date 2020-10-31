66118b913668d8b7d75cefc4b50d6d530c14b5c7
Undefinaids <nicolas.lemaire@epitech.eu>
2020-10-31 17:10:29 +0100

    Add add action

--- app/actions/add.py
+++ app/actions/add.py
@@ -1,5 +1,8 @@
 
 from actions.action import Action
+from utils import ask_question
+
+from harpokrat_client_library.models.domain.password import Password
 
 
 class Add(Action):
@@ -8,6 +11,19 @@ class Add(Action):
         super().__init__('add')
 
     def execute(self, hpk_api, args):
-        print("Executing {} action".format(self.label))
-        print("TODO")
+        max_confirm_error = 3
+        name = ask_question('New password name:')
+        login = ask_question('Login:')
+        password = ask_question('Password:', 'password')
+        while ask_question('Confirm password:', 'password') != password:
+            if max_confirm_error == 0:
+                print('Confirm password failed. Exit.')
+                return
+            print('Passwords do not match, please retry. ({} tries remaining)'.format(
+                max_confirm_error))
+            max_confirm_error -= 1
+        domain = ask_question('Domain:')
+        hpk_api.password_service.create(
+            Password(name, login, password, domain, private=False))
+        print("Success!")
         return
